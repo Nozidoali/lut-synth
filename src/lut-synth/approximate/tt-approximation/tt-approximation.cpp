d5ea3d420671d04f41acfe2f70fc56415b322f02
Hanyu Wang <nozidoali@gmail.com>
2026-03-31 00:52:24 -0700

    Extract shared error constraint helper and add assertions

--- src/lut-synth/approximate/tt-approximation/tt-approximation.cpp
+++ src/lut-synth/approximate/tt-approximation/tt-approximation.cpp
@@ -151,6 +151,7 @@ double bit_weight_for_output(uint32_t output_index, uint32_t m,
     }
     uint32_t offset = 0;
     for (uint32_t bw : register_bitsizes) {
+        assert(bw < 32);
         if (output_index < offset + bw) {
             uint32_t j = output_index - offset;
             return static_cast<double>(1u << (bw - 1 - j));
@@ -168,6 +169,7 @@ double max_error_per_minterm(uint32_t m,
     }
     double total = 0.0;
     for (uint32_t bw : register_bitsizes) {
+        assert(bw < 32);
         total += static_cast<double>((1u << bw) - 1);
     }
     return total;
@@ -243,6 +245,103 @@ PruningMask compute_pruning_mask(
 }
 
 #ifdef ENABLE_GUROBI
+
+void add_error_constraints(
+    GRBModel& model,
+    std::vector<GRBVar>& e_vars,
+    std::vector<std::vector<GRBVar>> const& d_vars,
+    std::vector<std::vector<int32_t>> const& f_values,
+    uint32_t m,
+    uint32_t num_minterms,
+    std::vector<double> const& weights,
+    double error_bound,
+    std::vector<uint32_t> const& register_bitsizes,
+    bool use_named_constraints) {
+
+    if (!register_bitsizes.empty()) {
+        uint32_t total = 0;
+        for (uint32_t bw : register_bitsizes) total += bw;
+        assert(total == m);
+    }
+
+    if (register_bitsizes.empty()) {
+        for (uint32_t x = 0; x < num_minterms; ++x) {
+            GRBLinExpr delta = 0;
+            for (uint32_t i = 0; i < m; ++i) {
+                int32_t coeff = (1 - 2 * f_values[i][x]) * (1 << (m - 1 - i));
+                delta += coeff * d_vars[i][x];
+            }
+            if (use_named_constraints) {
+                model.addConstr(e_vars[x] >= delta, "abs_pos_" + std::to_string(x));
+                model.addConstr(e_vars[x] >= -delta, "abs_neg_" + std::to_string(x));
+            } else {
+                model.addConstr(e_vars[x] >= delta);
+                model.addConstr(e_vars[x] >= -delta);
+            }
+        }
+    } else {
+        uint32_t R = register_bitsizes.size();
+        std::vector<std::vector<GRBVar>> er_vars(R);
+        for (uint32_t r = 0; r < R; ++r) {
+            er_vars[r].reserve(num_minterms);
+            double ub = static_cast<double>((1u << register_bitsizes[r]) - 1);
+            for (uint32_t x = 0; x < num_minterms; ++x) {
+                if (use_named_constraints) {
+                    er_vars[r].push_back(model.addVar(0.0, ub, 0.0, GRB_CONTINUOUS,
+                        "er_" + std::to_string(r) + "_" + std::to_string(x)));
+                } else {
+                    er_vars[r].push_back(model.addVar(0.0, ub, 0.0, GRB_CONTINUOUS));
+                }
+            }
+        }
+
+        uint32_t bit_offset = 0;
+        for (uint32_t r = 0; r < R; ++r) {
+            uint32_t bw = register_bitsizes[r];
+            for (uint32_t x = 0; x < num_minterms; ++x) {
+                GRBLinExpr delta_r = 0;
+                for (uint32_t j = 0; j < bw; ++j) {
+                    uint32_t i = bit_offset + j;
+                    int32_t coeff = (1 - 2 * f_values[i][x]) * (1 << (bw - 1 - j));
+                    delta_r += coeff * d_vars[i][x];
+                }
+                if (use_named_constraints) {
+                    model.addConstr(er_vars[r][x] >= delta_r,
+                        "abs_pos_r" + std::to_string(r) + "_" + std::to_string(x));
+                    model.addConstr(er_vars[r][x] >= -delta_r,
+                        "abs_neg_r" + std::to_string(r) + "_" + std::to_string(x));
+                } else {
+                    model.addConstr(er_vars[r][x] >= delta_r);
+                    model.addConstr(er_vars[r][x] >= -delta_r);
+                }
+            }
+            bit_offset += bw;
+        }
+
+        for (uint32_t x = 0; x < num_minterms; ++x) {
+            GRBLinExpr sum_er = 0;
+            for (uint32_t r = 0; r < R; ++r) {
+                sum_er += er_vars[r][x];
+            }
+            if (use_named_constraints) {
+                model.addConstr(e_vars[x] >= sum_er,
+                    "e_sum_" + std::to_string(x));
+            } else {
+                model.addConstr(e_vars[x] >= sum_er);
+            }
+        }
+    }
+
+    GRBLinExpr error_sum = 0;
+    for (uint32_t x = 0; x < num_minterms; ++x) {
+        error_sum += weights[x] * e_vars[x];
+    }
+    if (use_named_constraints) {
+        model.addConstr(error_sum <= error_bound, "error_bound");
+    } else {
+        model.addConstr(error_sum <= error_bound);
+    }
+}
 
 TTApproxResult solve_ilp(
     std::vector<kitty::dynamic_truth_table> const& exact_tts,
@@ -333,61 +432,8 @@ TTApproxResult solve_ilp(
         }
     }
 
-    if (params.register_bitsizes.empty()) {
-        for (uint32_t x = 0; x < num_minterms; ++x) {
-            GRBLinExpr delta = 0;
-            for (uint32_t i = 0; i < m; ++i) {
-                int32_t coeff = (1 - 2 * f_values[i][x]) * (1 << (m - 1 - i));
-                delta += coeff * d_vars[i][x];
-            }
-            model.addConstr(e_vars[x] >= delta, "abs_pos_" + std::to_string(x));
-            model.addConstr(e_vars[x] >= -delta, "abs_neg_" + std::to_string(x));
-        }
-    } else {
-        uint32_t R = params.register_bitsizes.size();
-        std::vector<std::vector<GRBVar>> er_vars(R);
-        for (uint32_t r = 0; r < R; ++r) {
-            er_vars[r].reserve(num_minterms);
-            double ub = static_cast<double>((1u << params.register_bitsizes[r]) - 1);
-            for (uint32_t x = 0; x < num_minterms; ++x) {
-                er_vars[r].push_back(model.addVar(0.0, ub, 0.0, GRB_CONTINUOUS,
-                    "er_" + std::to_string(r) + "_" + std::to_string(x)));
-            }
-        }
-
-        uint32_t bit_offset = 0;
-        for (uint32_t r = 0; r < R; ++r) {
-            uint32_t bw = params.register_bitsizes[r];
-            for (uint32_t x = 0; x < num_minterms; ++x) {
-                GRBLinExpr delta_r = 0;
-                for (uint32_t j = 0; j < bw; ++j) {
-                    uint32_t i = bit_offset + j;
-                    int32_t coeff = (1 - 2 * f_values[i][x]) * (1 << (bw - 1 - j));
-                    delta_r += coeff * d_vars[i][x];
-                }
-                model.addConstr(er_vars[r][x] >= delta_r,
-                    "abs_pos_r" + std::to_string(r) + "_" + std::to_string(x));
-                model.addConstr(er_vars[r][x] >= -delta_r,
-                    "abs_neg_r" + std::to_string(r) + "_" + std::to_string(x));
-            }
-            bit_offset += bw;
-        }
-
-        for (uint32_t x = 0; x < num_minterms; ++x) {
-            GRBLinExpr sum_er = 0;
-            for (uint32_t r = 0; r < R; ++r) {
-                sum_er += er_vars[r][x];
-            }
-            model.addConstr(e_vars[x] >= sum_er,
-                "e_sum_" + std::to_string(x));
-        }
-    }
-
-    GRBLinExpr error_sum = 0;
-    for (uint32_t x = 0; x < num_minterms; ++x) {
-        error_sum += weights[x] * e_vars[x];
-    }
-    model.addConstr(error_sum <= params.error_bound, "error_bound");
+    add_error_constraints(model, e_vars, d_vars, f_values, m, num_minterms,
+        weights, params.error_bound, params.register_bitsizes, true);
 
     GRBLinExpr obj = 0;
     for (uint32_t i = 0; i < m; ++i) {
@@ -555,57 +601,8 @@ TTApproxResult solve_ilp_ss_for_k(
         }
     }
 
-    if (params.register_bitsizes.empty()) {
-        for (uint32_t x = 0; x < num_minterms; ++x) {
-            GRBLinExpr delta = 0;
-            for (uint32_t i = 0; i < m; ++i) {
-                int32_t coeff = (1 - 2 * f_values[i][x]) * (1 << (m - 1 - i));
-                delta += coeff * d_vars[i][x];
-            }
-            model.addConstr(e_vars[x] >= delta);
-            model.addConstr(e_vars[x] >= -delta);
-        }
-    } else {
-        uint32_t R = params.register_bitsizes.size();
-        std::vector<std::vector<GRBVar>> er_vars(R);
-        for (uint32_t r = 0; r < R; ++r) {
-            er_vars[r].reserve(num_minterms);
-            double ub = static_cast<double>((1u << params.register_bitsizes[r]) - 1);
-            for (uint32_t x = 0; x < num_minterms; ++x) {
-                er_vars[r].push_back(model.addVar(0.0, ub, 0.0, GRB_CONTINUOUS));
-            }
-        }
-
-        uint32_t bit_offset = 0;
-        for (uint32_t r = 0; r < R; ++r) {
-            uint32_t bw = params.register_bitsizes[r];
-            for (uint32_t x = 0; x < num_minterms; ++x) {
-                GRBLinExpr delta_r = 0;
-                for (uint32_t j = 0; j < bw; ++j) {
-                    uint32_t i = bit_offset + j;
-                    int32_t coeff = (1 - 2 * f_values[i][x]) * (1 << (bw - 1 - j));
-                    delta_r += coeff * d_vars[i][x];
-                }
-                model.addConstr(er_vars[r][x] >= delta_r);
-                model.addConstr(er_vars[r][x] >= -delta_r);
-            }
-            bit_offset += bw;
-        }
-
-        for (uint32_t x = 0; x < num_minterms; ++x) {
-            GRBLinExpr sum_er = 0;
-            for (uint32_t r = 0; r < R; ++r) {
-                sum_er += er_vars[r][x];
-            }
-            model.addConstr(e_vars[x] >= sum_er);
-        }
-    }
-
-    GRBLinExpr error_sum = 0;
-    for (uint32_t x = 0; x < num_minterms; ++x) {
-        error_sum += weights[x] * e_vars[x];
-    }
-    model.addConstr(error_sum <= params.error_bound);
+    add_error_constraints(model, e_vars, d_vars, f_values, m, num_minterms,
+        weights, params.error_bound, params.register_bitsizes, false);
 
     GRBLinExpr obj = 0;
     for (uint32_t j = 0; j < num_hd; ++j) {
